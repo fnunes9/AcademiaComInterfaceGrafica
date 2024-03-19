20ee3b3760e8d54caabd5c854cf7ca818095519b
fnunesedutec <fernandanos@hotmail.com>
2024-03-19 11:50:14 -0300

    11h50m

--- src/main/java/com/mycompany/academiacominterfacegrafica/AcademiaComInterfaceGrafica.java
+++ src/main/java/com/mycompany/academiacominterfacegrafica/AcademiaComInterfaceGrafica.java
@@ -14,6 +14,22 @@ public class AcademiaComInterfaceGrafica extends JFrame {
         JFrame form=new JFrame("FitTech - Calcule sua mensalidade!" );
         JButton homem=new JButton("Homem");
         JButton mulher = new JButton("Mulher");
+        
+        // Configurando o botão Mensalidade Homem
+        homem.setBounds(110,200,150,50);
+        homem.setVisible(true);
+        
+        // Configurando o botão Mensalidade Mulher
+        mulher.setBounds(110,260,150,50);
+        mulher.setVisible(true);
+        
+        //Exibindo a janela
+        form.setBounds(100,200,400,600);
+        form.setLayout(null);
+        form.setVisible(true);
+        form.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        form.add(homem);
+        form.add(mulher);
     }
     
 }
