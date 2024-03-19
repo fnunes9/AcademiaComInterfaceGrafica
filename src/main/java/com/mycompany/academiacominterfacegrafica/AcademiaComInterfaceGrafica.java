fa223bd92602e6d76450714615cb7c42d5407bd8
fnunesedutec <fernandanos@hotmail.com>
2024-03-19 14:30:13 -0300

    14h30m

--- src/main/java/com/mycompany/academiacominterfacegrafica/AcademiaComInterfaceGrafica.java
+++ src/main/java/com/mycompany/academiacominterfacegrafica/AcademiaComInterfaceGrafica.java
@@ -12,28 +12,25 @@ import javax.swing.*;
 import java.awt.event.*;
 
 public class AcademiaComInterfaceGrafica extends JFrame {
-    
 
     public static void main(String[] args) {
-        JFrame form = new JFrame("FitTech - Calcule sua mensalidade!");
-        JButton homem = new JButton("Homem");
-        JButton mulher = new JButton("Mulher");
-        JSpinner entrada = new JSpinner(new SpinnerNumberModel(0, Integer.MIN_VALUE, Integer.MAX_VALUE, 100));
-        
+        JFrame form = new JFrame("FitTech - Calcule sua mensalidade!"); // Título do programa  
+        JButton homem = new JButton("Homem"); // Botão Homem
+        JButton mulher = new JButton("Mulher"); // Botão Mulher
 
         // Configurando o botão Mensalidade Homem
         homem.setBounds(110, 200, 150, 50);
         homem.setVisible(true);
         homem.addActionListener((ActionEvent e) -> {
-            homem.setText("Digite sua  idade");
-            int input = (int) entrada.getValue();
-            JOptionPane.showMessageDialog(null, input);
-            
+            JOptionPane.showMessageDialog(null, "Você clicou no botão Homem!");
         });
 
         // Configurando o botão Mensalidade Mulher
         mulher.setBounds(110, 260, 150, 50);
         mulher.setVisible(true);
+        mulher.addActionListener((ActionEvent e) -> {
+            JOptionPane.showMessageDialog(null, "Você clicou no botão Mulher");
+        });
 
         //Exibindo a janela
         form.setBounds(100, 200, 400, 600);
