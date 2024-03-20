03bf257a21411fb21e528c3ca3614d993f29d1cf
fnunesedutec <fernandanos@hotmail.com>
2024-03-20 11:56:05 -0300

    11h55m
    Boas práticas, passando os objetos para o método construtor.

--- src/main/java/com/mycompany/academiacominterfacegrafica/AcademiaComInterfaceGrafica.java
+++ src/main/java/com/mycompany/academiacominterfacegrafica/AcademiaComInterfaceGrafica.java
@@ -8,52 +8,40 @@ package com.mycompany.academiacominterfacegrafica;
  *
  * @author fernanda.nunes
  */
-import java.awt.Component;
 import javax.swing.*;
-import java.awt.event.*;
 
 public class AcademiaComInterfaceGrafica extends JFrame {
 
+    private final JButton homem;
+    private final JButton mulher;
+    
+    public AcademiaComInterfaceGrafica(){
+        homem=new JButton("Homem");
+        homem.setBounds(110, 200, 150, 50);
+        homem.setVisible(true);
+        add(homem);
+        
+        mulher=new JButton("Mulher");
+        mulher.setBounds(110, 260, 150, 50);
+        mulher.setVisible(true);
+        add(mulher);
+    }
+    
     
 
     public static void main(String[] args) {
-        JFrame form = new JFrame("FitTech - Calcule sua mensalidade!"); // Título do programa  
-        JButton homem = new JButton("Homem"); // Botão Homem
-        JButton mulher = new JButton("Mulher"); // Botão Mulher
-        JTextField entrada = new JTextField();
-
-        // Scanner entrada = new Scanner(System.in); // Dados a partir do teclado, temporário pois farei com IG
-        // Configurando o botão Mensalidade Homem
-        homem.setBounds(110, 200, 150, 50);
-        homem.setVisible(true);
-        homem.addActionListener((ActionEvent e) -> {
-            //int idadeHomem = entrada.nextInt();
-            Component idadeHomem = homem.add(entrada);
-           // int idadeHomem = Integer.parseInt(entrada.getText());
-            //int mensalidade = Cliente.CalculaMensalidade(idadeHomem, idadeHomem);
-            //System.out.println(Cliente.CalculaMensalidade(idadeHomem, idadeHomem));
-            //JOptionPane.showMessageDialog(null, mensalidade);
-        });
-
-        // Configurando o botão Mensalidade Mulher
-        mulher.setBounds(110, 260, 150, 50);
-        mulher.setVisible(true);
-        mulher.addActionListener((ActionEvent e) -> {
-            //int idadeMulher = entrada.nextInt();
-            //JOptionPane.showMessageDialog(null, idadeMulher);
-        });
-
+        AcademiaComInterfaceGrafica form = new AcademiaComInterfaceGrafica(); // Título do programa 
+        
         //Exibindo a janela
         form.setBounds(100, 200, 400, 600);
         form.setLayout(null);
         form.setVisible(true);
         form.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        form.add(homem);
-        form.add(mulher);
+        form.setTitle("FitTech - Calcule sua mensalidade!");
 
-        //Configurando campo de texto
+        /*Configurando campo de texto
         entrada.setBounds(110, 150, 150, 30);
-        entrada.setVisible(true);
+        entrada.setVisible(true);*/
     }
 
 }
