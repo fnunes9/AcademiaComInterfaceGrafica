087c7fdee348e3ce2ce0f47f3bc4cd2247b5b304
fnunesedutec <fernandanos@hotmail.com>
2024-03-20 16:01:22 -0300

    16h
    Consegui retornar o valor da mensalidade na interface. Falta melhorar a classe cliente e criar um método mais limpo.

--- src/main/java/com/mycompany/academiacominterfacegrafica/AcademiaComInterfaceGrafica.java
+++ src/main/java/com/mycompany/academiacominterfacegrafica/AcademiaComInterfaceGrafica.java
@@ -9,29 +9,52 @@ package com.mycompany.academiacominterfacegrafica;
  * @author fernanda.nunes
  */
 import javax.swing.*;
+import java.awt.event.*;
 
-public class AcademiaComInterfaceGrafica extends JFrame {
+public class AcademiaComInterfaceGrafica extends JFrame implements ActionListener {
 
     private final JButton homem;
     private final JButton mulher;
-    
-    public AcademiaComInterfaceGrafica(){
-        homem=new JButton("Homem");
-        homem.setBounds(110, 200, 150, 50);
-       // homem.setVisible(true);
+    private final JTextField idade;
+
+    public AcademiaComInterfaceGrafica() {
+        homem = new JButton("Homem");
+        homem.setBounds(110, 320, 150, 50);
         add(homem);
-        
-        mulher=new JButton("Mulher");
+        //homem.addActionListener(this);
+
+        mulher = new JButton("Mulher");
         mulher.setBounds(110, 260, 150, 50);
-        //mulher.setVisible(true);
         add(mulher);
+
+        add();
+
+        idade = new JTextField("Digite sua idade, depois clique no sexo");
+        idade.setBounds(55, 200, 250, 30);
+        add(idade);
     }
-    
-    
+
+    private void add() {
+        homem.addActionListener(this);
+        mulher.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == homem) {
+            String entradaIdade = idade.getText(); // entrada da idade por meio de string
+            int idadeHomem = Integer.parseInt(entradaIdade); //Convertendo String para int 
+            JOptionPane.showMessageDialog(this, "Valor da mensalidade R$ " + Cliente.CalculaMensalidade(idadeHomem, idadeHomem)); // Exibindo o valor da mensalidade
+        } else if (e.getSource() == mulher) {
+            String entradaIdade = idade.getText(); // entrada da idade por meio de string
+            int idadeMulher = Integer.parseInt(entradaIdade); //Convertendo String para int 
+            JOptionPane.showMessageDialog(this, "Valor da mensalidade R$ " + Cliente.CalculaMensalidade(idadeMulher, idadeMulher)); // Exibindo o valor da mensalidade
+        }
+    }
 
     public static void main(String[] args) {
         AcademiaComInterfaceGrafica form = new AcademiaComInterfaceGrafica(); // Título do programa 
-        
+
         //Exibindo a janela
         form.setBounds(100, 200, 400, 600);
         form.setLayout(null);
@@ -40,10 +63,6 @@ public class AcademiaComInterfaceGrafica extends JFrame {
         form.setResizable(false);
         form.setDefaultCloseOperation(EXIT_ON_CLOSE);
         form.setTitle("FitTech - Calcule sua mensalidade!");
-
-        /*Configurando campo de texto
-        entrada.setBounds(110, 150, 150, 30);
-        entrada.setVisible(true);*/
     }
 
 }
--- src/main/java/com/mycompany/academiacominterfacegrafica/Cliente.java
+++ src/main/java/com/mycompany/academiacominterfacegrafica/Cliente.java
@@ -3,31 +3,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.academiacominterfacegrafica;
+
 /**
  *
  * @author Fernanda
  */
 public class Cliente {
-    public static float CalculaMensalidade(int idadeHomem, int idadeMulher){
-        
-        if(idadeHomem <=15 && idadeMulher <=18){
+
+    public static float CalculaMensalidade(int idadeHomem, int idadeMulher) {
+
+        if (idadeHomem <= 15 && idadeMulher <= 18) {
             return 60;
-        }
-        else if(idadeHomem<=18){
+        } else if (idadeHomem <= 18) {
             return 75;
-        }
-        else if(idadeHomem<=30 && idadeMulher<=25){
+        } else if (idadeHomem <= 30 && idadeMulher <= 25) {
             return 90;
-        }
-        
-        else if(idadeHomem<=40 && idadeMulher<=40){
+        } else if (idadeHomem <= 40 && idadeMulher <= 40) {
             return 85;
-        }
-        else if(idadeHomem<=30 ){
+        } else if (idadeHomem <= 30) {
             return 80;
         }
         return 0;
     }
-    
-    
+
 }
